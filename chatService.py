30d79338a062cbc12b8dcb266cab2bf0216c16ef
piai <davidsomerville@comcast.net>
2025-11-29 13:44:25 -0500

    Added the ngScamp web app on the default route

--- chatService.py
+++ chatService.py
@@ -2,15 +2,25 @@ from langchain_ollama import ChatOllama
 from langchain.agents import create_agent
 from langchain_mcp_adapters.client import MultiServerMCPClient 
 from langsmith import traceable
-import asyncio 
-import time
+
 from dotenv import load_dotenv
+import asyncio 
+
+# Add in uvicorn web server and FastAPI web service module.
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 import uvicorn
 
 load_dotenv(dotenv_path=".env", override=True)
-
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Set up the model, using the chatOllama provider package
 llm = ChatOllama(
@@ -33,7 +43,7 @@ client = MultiServerMCPClient(
 
 # See https://docs.langchain.com/oss/python/langchain/mcp
 @traceable
-async def aichat(prompt:str):
+async def lcchat(prompt:str):
     tools =  await client.get_tools()  
 
     # Create a new agent
@@ -53,8 +63,21 @@ async def aichat(prompt:str):
 # Web service : e.g. http://localhost:8123/chat?prompt=How%20do%20I%20make%20scones 
 @app.get("/chat")
 async def chat(prompt: str):
-    response=await aichat(prompt)
+    response=await lcchat(prompt)
     return(response)
+
+# Note: Make sure this line is at the end of the file so fastAPI falls through the other
+# routes before serving up static files 
+app.mount("/", StaticFiles(directory="static", html=True), name="static")
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8123)
+
+"""
+Angular client Note:
+Use the following chat prompt to make an angular component to use this service
+Create an angular component that accepts a prompt string and sends it to a web service
+ in the format of http://piai:8123/chat?prompt={prompt}, this will return a
+  markdown formated string response that needs to be displayed to the user.
+"""
