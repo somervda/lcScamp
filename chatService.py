75247b2a231753bdba065fc98a498804de6c4999
piai <davidsomerville@comcast.net>
2026-01-07 09:05:38 -0500

    Added options for llm to connect to

--- chatService.py
+++ chatService.py
@@ -22,13 +22,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Set up the model, using the chatOllama provider package
-llm = ChatOllama(
-    model="granite4:3b",
-    base_url="http://jetai:11434",
-    num_ctx=16384,
-    temperature=0.1,
-)
+
 
 # I use the MultiServerMCPClient object to expose the mcpScamp service to the langchain agent
 client = MultiServerMCPClient(  
@@ -42,7 +36,30 @@ client = MultiServerMCPClient(
 
 # See https://docs.langchain.com/oss/python/langchain/mcp
 @traceable
-async def lcchat(prompt:str):
+async def lcchat(prompt:str,llmService:int):
+    # Set up the model, using the chatOllama provider package
+    match llmService:
+        case 1:
+            llm = ChatOllama(
+                model="granite4:3b",
+                base_url="http://jetai:11434",
+                num_ctx=16384,
+                temperature=0.1,
+            )
+        case 2:
+            llm = ChatOllama(
+                model="granite4:3b",
+                base_url="http://powerspec-g703:11434",
+                num_ctx=32768,
+                temperature=0.1,
+            )        
+        case 3:
+            llm = ChatOllama(
+                model="gpt-oss:20b",
+                base_url="http://mac:11434",
+                num_ctx=32768,
+                temperature=0.1,
+            )        
     tools =  await client.get_tools()  
 
     # Create a new agent
@@ -60,9 +77,11 @@ async def lcchat(prompt:str):
     return(scamp_response["messages"][-1].content)
 
 # Web service : e.g. http://localhost:8123/chat?prompt=How%20do%20I%20make%20scones 
+#  and http://piai:8123/chat?prompt=I%20was%20born%2019th%20of%20April%201959.%20How%20old%20am%20I%20today,%20based%20on%20the%20local%20date%20and%20time&llmService=2
+# http://localhost:8123/chat?prompt=How%20do%20I%20make%20scones 
 @app.get("/chat")
-async def chat(prompt: str):
-    response=await lcchat(prompt)
+async def chat(prompt: str,llmService:int=1):
+    response=await lcchat(prompt,llmService)
     return(response)
 
 # Note: Make sure this line is at the end of the file so fastAPI falls through the other
